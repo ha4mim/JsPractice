91809f79f922df93d0a864e9c1599938d1efb184
Sheikh-Hamim-Mahmud <sk.hamim.mahmud@gmail.com>
2022-07-04 08:32:26 +0600

    PracticeProblem1 Final Commit

---
+++ Day-1/PracticeProblem1/script.js
@@ -0,0 +1,8 @@
+var _totalTk = 500;
+var _fish = 110;
+var _ricePercent = ((40*5)/100);
+var _rice = _ricePercent*3;
+var _ricshaw = 40;
+var _totalExpence = _fish+_rice+_ricshaw;
+var _totalLeft = _totalTk - _totalExpence;
+document.write("Total Left: " +_totalLeft+ "Tk");
